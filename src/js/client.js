f51c324125cc49b67f09d387d76e3a4c9de0eb91
Audax Anchirinah <audax338@gmail.com>
2017-05-03 22:46:01 +0100

    Ediited the routes in client.js

--- src/js/client.js
+++ src/js/client.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import Archives from "./pages/Archives";
-import Featured from "./pages/Featured";
+import Sports from "./pages/Sports";
+import Gaming from "./pages/Gaming";
 import Layout from "./pages/Layout";
 import Settings from "./pages/Settings";
 
@@ -12,9 +12,9 @@ const app = document.getElementById('app');
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Layout}>
-      <IndexRoute component={Featured}></IndexRoute>
-      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
-      <Route path="settings" name="settings" component={Settings}></Route>
+      <IndexRoute component={Gaming}></IndexRoute>
+      <Route path="sports(/:article)" name="Sports" component={Sports}></Route>
+
     </Route>
   </Router>,
 app);
