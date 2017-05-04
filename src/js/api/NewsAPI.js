3ed859454be33a1b09424b45934f9a21d178f25d
Audax Anchirinah <audax338@gmail.com>
2017-05-04 18:26:17 +0100

    Created a function to fetch articles

--- src/js/api/NewsAPI.js
+++ src/js/api/NewsAPI.js
@@ -3,10 +3,11 @@ import Axios from 'axios';
 
 
 const NEWS_API_URL = 'https://newsapi.org/v1/sources?apiKey=213327409d384371851777e7c7f78dfe';
+const NEWS_API_URL_SOURCES = 'https://newsapi.org/v1/articles?apiKey=213327409d384371851777e7c7f78dfe&sortBy=top'
  export default {
-  getNews: function (category){
-    let encodedCategory = encodeURIComponent(category);
-    let requestUrl = `${NEWS_API_URL}&category=${encodedCategory}`;
+  getNews: function (){
+    const encodedCategory = encodeURIComponent();
+    const requestUrl = `${NEWS_API_URL}` //&category=${encodedCategory}`;
 
     return Axios.get(requestUrl).then(function(res){
       if(res.data.status === 'ok' && res.data.sources === []){
@@ -17,5 +18,20 @@ const NEWS_API_URL = 'https://newsapi.org/v1/sources?apiKey=213327409d3843718517
     },function(res){
       throw new Error('Error no news')
     })
+  },
+  getSources: function (category){
+    const encodedCategory = encodeURIComponent(category);
+    const requestUrl = `${NEWS_API_URL_SOURCES}&source=${encodedCategory}`;
+
+    return Axios.get(requestUrl).then(function(res){
+      if(res.data.status === 'ok' && res.data.articles === []){
+        throw new Error('Error no news');
+      }else {
+        return res.data.articles;
+      }
+    },function(res){
+      throw new Error('Error no news')
+    })
+
   }
 }
